14553d946347beb8e5a0a53e7be4b71b84c913e5
田搏文 <2392023097@qq.com>
2023-04-20 21:22:57 +0800

    初始化

---
+++ src/main/java/org/bowen/gms/GmsApp.java
@@ -0,0 +1,13 @@
+package org.bowen.gms;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bowen
+ */
+@SpringBootApplication
+public class GmsApp {
+    public static void main(String[] args) {
+        
+    }
+}
